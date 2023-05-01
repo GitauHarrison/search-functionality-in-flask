5fbc95095f066db957f1a659784913e0abae048c
harry <harrisongitau3@gmail.com>
2023-05-01 14:16:10 +0300

    Build: Handle search results
    - The search form is in the navbar, meaning it will be found in all templates
    - The before request is used to ensure that the search form is available for use
    - The g object from Flask is used to store the form object throughout the life of a request
    - The g variable is also specific to each request and each client making it ideal for multiple requests

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,39 @@
 from app import app, db
-from flask import render_template, url_for, session, request, flash, redirect
+from flask import render_template, url_for, session, request, flash, redirect, g
 from app.forms import PostForm
 from app.models import User, Post
+from app.forms import SearchForm
+
+# Executed before the a function
+@app.before_request
+def before_request():
+    g.search_form = SearchForm()
+
+
+@app.route('/search')
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('index'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(
+        g.search_form.q.data,
+        page,
+        app.config['POSTS_PER_PAGE'])
+    next_url = url_for(
+        'search',
+        q=g.search_form.q.data,
+        page=page + 1) if total > page * app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for(
+        'search',
+        q=g.search_form.a.data,
+        page=page - 1) if total > 1 else None
+    return render_template(
+        'search.html',
+        title='Search',
+        posts=posts,
+        next_url=next_url,
+        prev_url=prev_url
+    )
 
 
 @app.route('/', methods=['GET', 'POST'])

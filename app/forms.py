be1d8ff7a275bbb47f8d7f491f208f884b427b4c
harry <harrisongitau3@gmail.com>
2023-05-01 14:11:39 +0300

    Build: Define a search form
    - The form intentionally lacks a submit button
    - With a text field, this form will be submitted when user presses Enter
    - So, a button is not needed
    
    - The form is to submit a GET request, not POST request
    - The data in a GET form is in the URL of the request as args
    - request.args is therefore used, otherwise, it would have been request.form for a POST request
    - csrf is disabled to allow the search feature to work

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
+from flask import request
 
 
 class PostForm(FlaskForm):
@@ -8,3 +9,20 @@ class PostForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+class SearchForm(FlaskForm):
+    q = StringField(
+        'Search',
+        validators=[DataRequired()],
+        render_kw={'placeholder': 'Search ...'})
+
+    def __init__(self, *args, **kwargs):
+        # formdata is where Flask gets form submissions
+        if 'formdata' not in kwargs:
+            # GET request: request.args, POST: request.form
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            # Disable CSRF for clickable link
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
